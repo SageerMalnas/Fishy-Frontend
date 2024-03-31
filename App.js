bea99c9548dd4c267d206b3ac78902173463b86e
Rutuja2024 <rutujahujare20@gmail.com>
2024-03-31 15:50:02 +0530

    Design Changes

--- App.js
+++ App.js
@@ -3,13 +3,14 @@
 // import { createDrawerNavigator } from "@react-navigation/drawer";
 // import OpenCamera from "./src/OpenCamera.js";
 // import HomeScreen from "./src/HomeScreen";
-
+// import TabNavigator from './src/TabNavigator'
 // import LoginScreen from "./src/LoginScreen";
 // import SignupScreen from "./src/SignInScreen";
 // import FishEcom from "./src/FishEcom/FishEcomScreen.js";
 // import CustomDrawerContent from "./src/CustomDrawerContent"; 
 // import Shop from './src/Shop';
 
+// import Ionicons from 'react-native-vector-icons/Ionicons';
 // import {StatusBar} from 'react-native';
 // import {createStackNavigator} from '@react-navigation/stack';
 
@@ -35,66 +36,107 @@
 
 // function DrawerNavigator() {
 //   return (
-//     <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <CustomDrawerContent {...props} />}>
-//       <Drawer.Screen name="Home" component={HomeScreen} />
+//     // <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <CustomDrawerContent {...props} />}>
+//     <Drawer.Navigator
+//       drawerContent={props => <CustomDrawerContent {...props} />}
+//       screenOptions={{
+//         headerShown: false,
+//         drawerActiveBackgroundColor: '#aa18ea',
+//         drawerActiveTintColor: '#fff',
+//         drawerInactiveTintColor: '#333',
+//         drawerLabelStyle: {
+//           marginLeft: -25,
+//           fontFamily: 'Roboto-Medium',
+//           fontSize: 15,
+//         },
+//       }}>
+//          <Drawer.Screen
+//         name="Home"
+//         component={TabNavigator}
+//         options={{
+//           drawerIcon: ({color}) => (
+//             <Ionicons name="home-outline" size={22} color={color} />
+//           ),
+//         }}
+//       />
+    
     
 //       <Drawer.Screen name="OpenCamera" component={OpenCamera} />
 //       <Drawer.Screen name="Shop" component={Shop} /> 
 //       <Drawer.Screen name="Login" component={LoginScreen} />
 //       <Drawer.Screen name="Signup" component={SignupScreen} />
-//       <Drawer.Screen name="Fish Ecommerce" component={FishEcom}/>
+//       <Drawer.Screen name="Fish Ecommerce" component={FishEcom}/> 
 //     </Drawer.Navigator>
 //   );
 // }
 
 
 
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createStackNavigator } from '@react-navigation/stack';
+// import * as React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+// import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from "./src/HomeScreen";
-import LoginScreen from "./src/LoginScreen";
-import SignupScreen from "./src/SignInScreen";
-import OpenCamera from "./src/OpenCamera";
-import FishEcom from "./src/FishEcom/FishEcomScreen";
-import Shop from './src/Shop';
-import CustomDrawerContent from "./src/CustomDrawerContent";
+// import HomeScreen from "./src/HomeScreen";
+// import LoginScreen from "./src/LoginScreen";
+// import SignupScreen from "./src/SignInScreen";
+// import OpenCamera from "./src/OpenCamera";
+// import FishEcom from "./src/FishEcom/FishEcomScreen";
+// import Shop from './src/Shop';
+// import CustomDrawerContent from "./src/CustomDrawerContent";
 
-const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
+// const Drawer = createDrawerNavigator();
+// const Stack = createStackNavigator();
 
-function DrawerNavigator() {
-  return (
-    <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <CustomDrawerContent {...props} />}>
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="OpenCamera" component={OpenCamera} />
-      <Drawer.Screen name="Shop" component={Shop} />
-      <Drawer.Screen name="Fish Ecommerce" component={FishEcom}/>
-    </Drawer.Navigator>
-  );
-}
+// function DrawerNavigator() {
+//   return (
+//     <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <CustomDrawerContent {...props} />}>
+//       <Drawer.Screen name="Home" component={HomeScreen} />
+//       <Drawer.Screen name="OpenCamera" component={OpenCamera} />
+//       <Drawer.Screen name="Shop" component={Shop} />
+//       <Drawer.Screen name="Fish Ecommerce" component={FishEcom}/>
+//     </Drawer.Navigator>
+//   );
+// }
 
-function AuthNavigator() {
-  return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Signup" component={SignupScreen} />
-    </Stack.Navigator>
-  );
-}
+// function AuthNavigator() {
+//   return (
+//     <Stack.Navigator initialRouteName="Login">
+//       <Stack.Screen name="Login" component={LoginScreen} />
+//       <Stack.Screen name="Signup" component={SignupScreen} />
+//     </Stack.Navigator>
+//   );
+// }
 
-export default function App() {
-  const [isUserLoggedIn, setIsUserLoggedIn] = React.useState(true); 
+// export default function App() {
+//   const [isUserLoggedIn, setIsUserLoggedIn] = React.useState(true); 
 
+//   return (
+//     <NavigationContainer>
+//       {isUserLoggedIn ? (
+//         <DrawerNavigator />
+//       ) : (
+//         <AuthNavigator />
+//       )}
+//     </NavigationContainer>
+//   );
+// }
+
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import AuthStack from './src/navigation/AuthStack';
+import AppStack from './src/navigation/AppStack';
+
+function App() {
   return (
     <NavigationContainer>
-      {isUserLoggedIn ? (
-        <DrawerNavigator />
-      ) : (
-        <AuthNavigator />
-      )}
+      <AppStack />
+      {/* <AuthStack /> */}
     </NavigationContainer>
   );
 }
+
+export default App;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,123 @@
+import React, { useState, useRef, useEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+} from "react-native";
+import Carousel from "react-native-snap-carousel";
+import Feather from "react-native-vector-icons/Feather";
+
+import BannerSlider from "../components/BannerSlider";
+import { windowWidth } from "../utils/Dimensions";
+
+import { freeGames, paidGames, sliderData } from "../model/data";
+import CustomSwitch from "../components/CustomSwitch";
+import ListItem from "../components/ListItem";
+
+export default function HomeScreen({ navigation }) {
+  const [gamesTab, setGamesTab] = useState(1);
+
+  const renderBanner = ({ item, index }) => {
+    return <BannerSlider data={item} />;
+  };
+
+  const onSelectSwitch = (value) => {
+    setGamesTab(value);
+  };
+  const titleAnim = useRef(new Animated.Value(-100)).current;
+
+  useEffect(() => {
+    Animated.timing(titleAnim, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, [titleAnim]);
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff", paddingTop: 40 }}>
+      <ScrollView style={{ paddingHorizontal: 20 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginBottom: 20,
+          }}
+        >
+          <Text style={{ fontSize: 24, fontFamily: "Roboto-Medium" }}>
+            Hello Rutuja
+          </Text>
+          <TouchableOpacity onPress={() => navigation.openDrawer()}>
+            <ImageBackground
+              source={require("../assets/images/user-profile.jpg")}
+              style={{ width: 45, height: 45 }}
+              imageStyle={{ borderRadius: 25 }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.container}>
+          <Animated.Text
+            style={[styles.title, { transform: [{ translateX: titleAnim }] }]}
+          >
+            Fish Species Identification
+          </Animated.Text>
+          <Text style={styles.subtitle}>and Disease Detection</Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Home")}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Let's Get Started</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: "#1d3557",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 18,
+    color: "black",
+    marginBottom: 30,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#1d3557",
+    padding: 10,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 10,
+  },
+});
