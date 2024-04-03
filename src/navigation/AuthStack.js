da845d6f3127c7572fed61d80eb6a5543ae273bf
SageerMalnas <sageermalnas78@gmail.com>
2024-04-03 18:04:57 +0530

    Added Loader

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,21 +1,75 @@
-import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import React, { useState, useEffect } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import AppStack from '../navigation/AppStack';
+import Loader from '../screens/Loader'; // Import the Loader component
+
 const Stack = createNativeStackNavigator();
 
 const AuthStack = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
-      <Stack.Screen name="AppStack" component={AppStack} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {isLoading ? (
+        <Stack.Screen name="Loader" component={Loader} />
+      ) : (
+        <>
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="AppStack" component={AppStack} />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
 
 export default AuthStack;
+
+
+// import React, { useState, useEffect } from 'react';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import OnboardingScreen from '../screens/OnboardingScreen';
+// import LoginScreen from '../screens/LoginScreen';
+// import RegisterScreen from '../screens/RegisterScreen';
+// import AppStack from '../navigation/AppStack';
+// import Loader from '../screens/Loader'; // Import the Loader component
+
+// const Stack = createNativeStackNavigator();
+
+// const AuthStack = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     // Simulate loading delay
+//     setTimeout(() => {
+//       setIsLoading(false);
+//     }, 1000);
+//   }, []);
+
+//   return (
+//     <Stack.Navigator screenOptions={{ headerShown: false }}>
+//       <Stack.Screen name="Loader" component={Loader} />
+//       {!isLoading && (
+//         <>
+//           <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+//           <Stack.Screen name="Login" component={LoginScreen} />
+//           <Stack.Screen name="Register" component={RegisterScreen} />
+//           <Stack.Screen name="AppStack" component={AppStack} />
+//         </>
+//       )}
+//     </Stack.Navigator>
+//   );
+// };
+
+// export default AuthStack;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,36 +1,23 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
   SafeAreaView,
   ScrollView,
   ImageBackground,
-  TextInput,
   TouchableOpacity,
   StyleSheet,
   Animated,
+  Dimensions, // Import Dimensions from 'react-native'
 } from "react-native";
-import Carousel from "react-native-snap-carousel";
-import Feather from "react-native-vector-icons/Feather";
+import { windowWidth, windowHeight } from "../utils/Dimensions";
+import { FontAwesome5 } from '@expo/vector-icons'; // Import FontAwesome5 for fish icon
 
-import BannerSlider from "../components/BannerSlider";
-import { windowWidth } from "../utils/Dimensions";
-
-import { freeGames, paidGames, sliderData } from "../model/data";
-import CustomSwitch from "../components/CustomSwitch";
-import ListItem from "../components/ListItem";
+const { width, height } = Dimensions.get('window');
 
 export default function HomeScreen({ navigation }) {
   const [gamesTab, setGamesTab] = useState(1);
-
-  const renderBanner = ({ item, index }) => {
-    return <BannerSlider data={item} />;
-  };
-
-  const onSelectSwitch = (value) => {
-    setGamesTab(value);
-  };
-  const titleAnim = useRef(new Animated.Value(-100)).current;
+  const titleAnim = useState(new Animated.Value(-100))[0]; // Change to useState hook
 
   useEffect(() => {
     Animated.timing(titleAnim, {
@@ -38,51 +25,59 @@ export default function HomeScreen({ navigation }) {
       duration: 1000,
       useNativeDriver: true,
     }).start();
-  }, [titleAnim]);
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#f0f8ff", paddingTop: 40 }}>
-      <ScrollView style={{ paddingHorizontal: 20  } } isVerticalScrollBarEnabled ={false}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: 20,
-          }}
-        >
-          <Text style={{ fontSize: 24, fontFamily: "Roboto-Medium"}}>
-            Hello Rutuja
-          </Text>
-          <TouchableOpacity onPress={() => navigation.openDrawer()}>
-            <ImageBackground
-              source={require("../assets/images/user-profile.jpg")}
-              style={{ width: 45, height: 45 }}
-              imageStyle={{ borderRadius: 25 }}
-            />
-          </TouchableOpacity>
-        </View>
+  }, []);
 
-        <View style={styles.container}>
-          <Animated.Text
-            style={[styles.title, { transform: [{ translateX: titleAnim }] }]}
+  return (
+    <ImageBackground
+      source={require("../../assets/unnamed.png")}
+      style={[styles.background, { width: "100%", height:"100%" }]} // Adjust the width and height
+      resizeMode="cover"
+    >
+      <SafeAreaView style={{ flex: 1, backgroundColor: "transparent", paddingTop: 40 }}>
+        <ScrollView style={{ paddingHorizontal: 20 }} isVerticalScrollBarEnabled={false}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBottom: 20,
+            }}
           >
-            Fish Species Identification
-          </Animated.Text>
-          <Text style={styles.subtitle}>and Disease Detection</Text>
-          <TouchableOpacity
-            onPress={() => navigation.navigate("Home")}
-            style={styles.button}
-          >
-            <Text style={styles.buttonText}>Let's Get Started</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+            <Text style={{ fontSize: 24, fontFamily: "Roboto-Medium" }}>
+              Hello Rutuja
+            </Text>
+            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+              <ImageBackground
+                source={require("../assets/images/user-profile.jpg")}
+                style={{ width: 45, height: 45 }}
+                imageStyle={{ borderRadius: 25 }}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.container}>
+            <Animated.Text
+              style={[styles.title, { transform: [{ translateX: titleAnim }] }]}
+            >
+              Fish Species Identification
+            </Animated.Text>
+            <Text style={styles.subtitle}>and Disease Detection</Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("OpenCamera")}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Let's Get Started</Text>
+            </TouchableOpacity>
+            {/* Add fish icon */}
+            <FontAwesome5 name="fish" size={50} color="blue" style={styles.icon} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
+
 const styles = StyleSheet.create({
   background: {
-    flex: 1,
-    backgroundColor: "#1d3557",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -115,9 +110,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontWeight: "bold",
   },
-  logo: {
-    width: 150,
-    height: 150,
-    marginBottom: 10,
+  icon: {
+    marginTop: 20,
   },
 });
