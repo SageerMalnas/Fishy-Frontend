2c6e9b4a400b99df44722dc73ae44ac89f6c5b6f
SageerMalnas <sageermalnas78@gmail.com>
2024-04-04 21:58:45 +0530

    home screen

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+// Header.js
+import React from 'react';
+import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
+
+const Header = ({ navigation }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 50,
+        marginBottom: 20,
+        paddingHorizontal: 20,
+      }}
+    >
+      <Text style={{ fontSize: 24, fontFamily: "Roboto-Medium", color: "white" }}>
+        Hello Rutuja
+      </Text>
+      <TouchableOpacity onPress={() => navigation.openDrawer()}>
+        <ImageBackground
+          source={require("./assets/images/user-profile.jpg")}
+          style={{ width: 45, height: 45 }}
+          imageStyle={{ borderRadius: 25 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Header;
+
